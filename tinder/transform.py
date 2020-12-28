c770cde1170e3a41707595cd44079b67b1da9824
mldangelo <michael.l.dangelo@gmail.com>
2020-12-28 13:52:01 -0500

    moved everything into a subpath

--- tinder/transform.py
+++ tinder/transform.py
@@ -1,10 +1,15 @@
 import glob
 import pandas as pd
 
-matches = sorted(glob.glob('./data/matches*.json'), reverse=True) # most recent index
+matches = sorted(glob.glob('./data/matches-*.json'), reverse=True)[0] # most recent index
 print(matches)
 
 messages = sorted(glob.glob('./data/messages/*-*.json'))
 print(len(messages))
 unique_messages = { m.split('-')[0]: m for m in messages } # most recent unique dump
 print(len(unique_messages))
+
+print(pd.read_json(messages[0]))
+
+# for m in unique_messages:
+#    read_json
